da81e9f3f625f440761a9706a0dba63ffebc0fbd
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-28 20:10:06 +0200

    Snack_2:Add soccer players array with initial properties and log to console

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,3 +4,60 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
  */
+
+console.log(`hi`);
+
+const soccerPlayers = [
+    {
+        name: 'Kylian Mbappé',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Erling Haaland',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Jude Bellingham',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Vinícius Júnior',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Harry Kane',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Lautaro Martínez',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Pedri González',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Mohamed Salah',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Declan Rice',
+        score: 0,
+        fouls_won:0,
+    },
+    {
+        name: 'Lamine Yamal',
+        score: 0,
+        fouls_won:0,
+    },
+]
+
+console.log(soccerPlayers)
