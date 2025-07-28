7f2e67b154a331bd826de777c0e90c861630cde9
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-28 19:19:45 +0200

    Snack_1: Add array of bike objects with name and weight properties

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,3 +1,46 @@
 /* Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore. */
+
+const biciclette = [
+    {
+        name: 'Tarmac SL8',
+        weight: 6.65,
+    },
+    {
+        name: 'Trek SLR9',
+        weight: 6.79,
+    },
+    {
+        name: 'Cannondale',
+        weight: 6.85,
+    },
+    {
+        name: 'Canyon Ultimate',
+        weight: 6.73,
+    },
+    {
+        name: 'Teammachine SLR01',
+        weight: 6.90,
+    },
+    {
+        name: 'Pinarello Dogma',
+        weight: 6.80,
+    },
+    {
+        name: 'Scott Addict',
+        weight: 6.78,
+    },
+    {
+        name: 'Orbea Orca',
+        weight: 6.95,
+    },
+    {
+        name: 'Wilier Filante',
+        weight: 6.80,
+    },
+    {
+        name: 'Cervélo R5',
+        weight: 7.10,
+    },
+]
